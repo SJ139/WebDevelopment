2bacb6901a6ac17afbf85c449c43ba07007ac11a
SJ139 <90908149+SJ139@users.noreply.github.com>
2022-08-06 07:23:17 -0400

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 var newTasks = [];
+let workItems=[];
 
 app.set('view engine', 'ejs');
 
@@ -22,17 +23,38 @@ app.get("/", function(req, res) {
       };
       var day = today.toLocaleDateString("en-US", options);
 
-        res.render("list", {dayOfWeek: day, newListItem:newTasks});
+        res.render("list", {listTitle: day, newListItem:newTasks});
       });
 
       // Post method - takes the input from newTask in list.ejs form and writes to console
-      app.post("/", function(req,res){
-        var newTask = req.body.newItem;
-        newTasks.push(newTask);
+      // the if statement is supposed to create seperat post to 2 lists
 
-        res.redirect("/");
+      app.post("/", function(req,res){
+        let newTask = req.body.newItem;
+
+        if(req.body.list == "work"){
+          workItems.push(newTask);
+          res.redirect("/work");
+        }else{
+          newTasks.push(newTask);
+          res.redirect("/");
+        }
 
       });
+
+      app.get("/work", function(req,res){
+        res.render("list",{listTitle:"Work List", newListItem:workItems});
+      });
+
+      app.post("/work", function(req,res){
+        let item = req.body.newItem;
+        workItems.push(item);
+        res.redirect("/work");
+      });
+
+      app.get("/about", function(req,res){
+        res.render("about", {listTitle:"About"});
+      })
 
     app.listen(3000, function() {
       console.log("Server started on port 3000.");
